8f5822463177e90245e7b9664b58825c0434174a
StevenProgrammer <steven.josspa@gmail.com>
2021-10-03 20:30:59 -0600

    Agregué más lugares y medidas

--- AlgoritmoBase.py
+++ AlgoritmoBase.py
@@ -13,30 +13,38 @@ def Recomendacion(temp): #Función para determinar el nivel de estrés térmico
     if temp >= 40.0: #Grados Celsius
         print(Lugar0, "Esta en condición Roja de acuerdo a la escala de estrés térmico con una temperatura de=",'%.1f' %temp,"C \n"
                      "Le recomendamos seguir estas medidas:\n"
-                     "No estar más de 30 minutos bajo el sol\n"
-                     "Use bloqueador solar y si sale lleve protección para la piel\n"
-                     "Tome agua aproximadamente 2 litros al día\n"
-                     "Recuerde que las horas más calientes son al medio día")
+                     "1.Si presenta mucho sudor, cansancio, piel irritada, sed, \n"
+                     "tome un descanso a la sombra he hidrátese\n"
+                     "2.Use bloqueador solar y si sale lleve protección para la piel\n"
+                     "3.Use sombrero y ropa holgada que proteja su piel\n"
+                     "4.Tomar un vaso de agua cada 15 minutos cerca de las horas de más calor\n"
+                     "5.No trabaje expuesto al sol entre las 10 am y las 4 pm\n"
+                     "6.Aliméntese adecuadamente\n"
+                     "7.Haga trabajo de intensidad baja, sobre todo cerca de las horas de más calor")
 
 
     elif temp < 40.0 and temp >= 30.0:
         print(Lugar0, "Esta en condición Amarilla de acuerdo a la escala de estrés térmico con una temperatura de=",'%.1f' %temp,"C \n"
-                     "Le recomendamos seguir estas medidas:\n"
-                     "No estar más de 1 hora bajo el sol\n"
-                     "Use bloqueador solar y si sale lleve protección para la piel\n"
-                     "Tome agua aproximadamente 2 litros al día\n"
-                     "Recuerde que las horas más calientes son al medio día")
+                     "1.Si presenta mucho sudor, cansancio, piel irritada, sed, \n"
+                     "tome un descanso a la sombra he hidrátese\n"
+                     "2.Use bloqueador solar y si sale lleve protección para la piel\n"
+                     "3.Use sombrero y ropa holgada que proteja su piel\n"
+                     "4.Tome un vaso de agua aproximadamente cada 30 minutos cerca de las horas de más calor\n"
+                     "5.No trabaje expuesto al sol entre las 10 am y las 3 pm.\n"
+                     "6.Haga trabajo de intensidad moderada, sobre todo cerca de las horas de más calor")
+
 
     elif temp < 30.0:
         print(Lugar0, "Esta en condición Verde de acuerdo a la escala de estrés térmico con una temperatura de=",'%.1f' %temp,"C \n"
-                     "Le recomendamos seguir estas medidas:\n"
-                     "Puede estar bajo el sol no más de 5 horas\n"
-                     "Use bloqueador solar y si sale lleve protección para la piel\n"
-                     "Tome agua aproximadamente 2 litros al día\n"
-                     "Recuerde que las horas más calientes son al medio día")
+                     "1.Si presenta mucho sudor, cansancio, piel irritada, sed, \n"
+                     "tome un descanso a la sombra he hidrátese\n"
+                     "2.Use bloqueador solar y si sale lleve protección para la piel\n"
+                     "3.Use sombrero y ropa holgada que proteja su piel\n"
+                     "4.Tome agua aproximadamente 2 litros al día\n"
+                     "5.No trabaje expuesto al sol entre las 11 am y las 3 pm")
 
 
-Liberia = 3623076 #Lista ciudades por código para buscar en API
+Liberia = 3623076 #Lista ciudades de Guanacaste por código para buscar en API
 Nicoya = 3622716
 SantaCruz = 3621607
 Belen = 3624718
@@ -44,12 +52,16 @@ Sardinal = 3621490
 Hojancha = 3623454
 Bagaces = 3624822
 Cañas = 3624468
+LaCruz = 3623258
+Carrillo = 3624379
+Nandayure = 3622765
+
 
 Lugar0 = input("Agregue el distrito guanacasteco del que quieres saber: ")
 Lugar = Lugar0.lower()
 Lugar = Lugar.replace(" ", "")
 
-Lista = [Liberia, Nicoya, SantaCruz, Belen, Sardinal, Hojancha, Bagaces, Cañas]
+Lista = [Liberia, Nicoya, SantaCruz, Belen, Sardinal, Hojancha, Bagaces, Cañas, LaCruz, Carrillo, Nandayure]
 
 #Lista de if donde integro las funciones y las ejecuto
 
@@ -76,6 +88,12 @@ elif Lugar == "bagaces":
 
 elif Lugar == "cañas":
     Recomendacion(temperatura_ciudad(Lista[7],))
+elif Lugar == "lacruz":
+    Recomendacion(temperatura_ciudad(Lista[8],))
+elif Lugar == "carrillo":
+    Recomendacion(temperatura_ciudad(Lista[9],))
+elif Lugar == "nandayure":
+    Recomendacion(temperatura_ciudad(Lista[10],))
 
 else:
     print("Lo sentimos el lugar que ingreso no está disponible, o escriba el nombre correctamente")
